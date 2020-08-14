2ef992023d711a1e6848f020574a90950ffe1bc6
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-08-15 01:45:20 +0530

    Genserver boundary layer added

--- lib/boundary.ex
+++ lib/boundary.ex
@@ -1,8 +1,7 @@
 defmodule Calculator.Boundary do
   alias Calculator.Core
-
   def start(initial_state) do
-    spawn(fn -> run(initial_state) end)
+    spawn(fn -> run(initial_state) end) # <--- init
   end
 
   def run(state) do
@@ -14,18 +13,18 @@ defmodule Calculator.Boundary do
   def listen(state) do
     receive do
       {:add, number} ->
-        Core.add(state, number)
+        Core.add(state, number) # handle_cast
       {:subtract, number} ->
-        Core.subtract(state, number)
+        Core.subtract(state, number) # handle_cast
       {:multiply, number} ->
-        Core.multiply(state, number)
+        Core.multiply(state, number) # handle_cast
       {:divide, number} ->
-        Core.divide(state, number)
-      :clear ->
-        0
+        Core.divide(state, number) # handle_cast
+      {:custom, f, number} ->
+        Core.custom(state, f, number) # handle_cast
       {:state, pid} ->
-        send(pid, {:state, state})
-        state
+        send(pid, {:state, state}) # handle_call
+      state
     end
   end
 end
